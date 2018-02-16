085fe527cd73446046a67df8f34d2d8db30136d9
Zach Overhulser <zoverhulser@gmail.com>
2018-02-16 09:49:27 -0600

    Greets user in Russian.

--- index.mjs
+++ index.mjs
@@ -4,7 +4,11 @@ export function makeGreeter(language) {
             return "\u00A1Hola" + " " + name + "!";
         };
     }
-
+    if (language === "Russian") {
+        return function (name = "\u043c\u0438\u0440") {
+            return "\u041F\u0440\u0438\u0432\u0435\u0442\u0020" + name + "!";
+        };
+    }
     return function (name = "world") {
         return "Hello " + name + "!";
     };
