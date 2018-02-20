21daffa94e80119a77bce791043738614127fc35
Mike Ricos <mike.ricos@gmail.com>
2018-02-20 11:47:21 -0600

    Substitutes Russian 'world' string via global dictionary array.

--- index.mjs
+++ index.mjs
@@ -1,3 +1,14 @@
+const dictionary = [
+    {
+        language: "English",
+        world: "world"
+    },
+    {
+        language: "Russian",
+        world: "\u043c\u0438\u0440" // UTF-8
+    }
+];
+
 export function makeGreeter(language) {
     if (language === "Spanish") {
         return function (name = "Mundo") {
@@ -5,8 +16,9 @@ export function makeGreeter(language) {
         };
     }
     if (language === "Russian") {
-        return function (name = "\u043c\u0438\u0440") {
-            return "\u041F\u0440\u0438\u0432\u0435\u0442\u0020" + name + "!";
+        return function (name = dictionary[1].world) {
+            return "\u041F\u0440\u0438\u0432\u0435\u0442\u0020"
+                    + name + "!";
         };
     }
     return function (name = "world") {
