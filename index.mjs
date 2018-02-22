03958dfa69312816d30a856512bc1baad7e8186b
Zach Overhulser <zoverhulser@gmail.com>
2018-02-22 14:04:09 -0600

    Replaces unicode characters with unicode escape sequences.

--- index.mjs
+++ index.mjs
@@ -2,14 +2,14 @@ const worldInDifferentLanguages = {
     "English": "world",
     "Russian": "\u043c\u0438\u0440", // мир
     "Spanish": "Mundo",
-    "Greek": "κόσμος"
+    "Greek": "\u03BA\u03CC\u03C3\u03BC\u03BF\u03C2" //κόσμος
 };
 
 const helloInDifferentLanguages = {
     "English": "Hello",
     "Russian": "\u041F\u0440\u0438\u0432\u0435\u0442", //Привет
     "Spanish": "\u00A1Hola", //¡Hola
-    "Greek": "γειά σου"
+    "Greek": "\u03B3\u03B5\u03B9\u03AC \u03C3\u03BF\u03BD" //γειά σου
 };
 
 export function makeGreeter(language) {
--- test.mjs
+++ test.mjs
@@ -18,8 +18,8 @@ assert.equal(
     greet.spanish("Andr\u00E9s"),
     "\u00A1Hola Andr\u00E9s!"
 );
-assert.equal(greet.russian(), "Привет мир!");
-assert.equal(greet.greek(), "γειά σου κόσμος!");
+assert.equal(greet.russian(), "\u041F\u0440\u0438\u0432\u0435\u0442 \u043c\u0438\u0440!");
+assert.equal(greet.greek(), "\u03B3\u03B5\u03B9\u03AC \u03C3\u03BF\u03BD \u03BA\u03CC\u03C3\u03BC\u03BF\u03C2!");
 
 
 console.log("success");
