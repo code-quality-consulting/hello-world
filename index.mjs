ccbeced48e70deb06b2ade5789131881499d39e8
Zach Overhulser <zoverhulser@gmail.com>
2018-02-26 11:14:33 -0600

    Replaces UTF-8 escape sequences with UTF-8 characters in index.

--- index.mjs
+++ index.mjs
@@ -1,15 +1,15 @@
 const worldInDifferentLanguages = {
     "English": "world",
-    "Russian": "\u043c\u0438\u0440", // мир
+    "Russian": "мир",
     "Spanish": "Mundo",
-    "Greek": "\u03BA\u03CC\u03C3\u03BC\u03BF\u03C2" //κόσμος
+    "Greek": "κόσμος"
 };
 
 const helloInDifferentLanguages = {
     "English": "Hello",
-    "Russian": "\u041F\u0440\u0438\u0432\u0435\u0442", //Привет
-    "Spanish": "\u00A1Hola", //¡Hola
-    "Greek": "\u03B3\u03B5\u03B9\u03AC \u03C3\u03BF\u03BD" //γειά σου
+    "Russian": "Привет",
+    "Spanish": "¡Hola",
+    "Greek": "γειά σον"
 };
 
 export function makeGreeter(language) {
