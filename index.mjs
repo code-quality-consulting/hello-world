9f0c9bccd57e88ce43ada837a01091f41cfc4c41
Ben Pardo <visaudi@gmail.com>
2018-04-04 13:47:51 -0500

    Greets user in Arabic.

--- index.mjs
+++ index.mjs
@@ -1,3 +1,5 @@
+const rle = "\u202b";
+
 const worldInDifferentLanguages = {
     "English": "world",
     "Russian": "мир",
@@ -8,7 +10,8 @@ const worldInDifferentLanguages = {
     "Dutch": "wereld",
     "Korean": "세계",
     "Japanese": "世界",
-    "Chinese": "世界"
+    "Chinese": "世界",
+    "Arabic": "بالعالم"
 };
 
 const helloInDifferentLanguages = {
@@ -21,11 +24,15 @@ const helloInDifferentLanguages = {
     "Dutch": "Hallo",
     "Korean": "여보세요",
     "Japanese": "こんにちは",
-    "Chinese": "你好"
+    "Chinese": "你好",
+    "Arabic": "مرحبا"
 };
 
 export function makeGreeter(language) {
     return function (name = worldInDifferentLanguages[language]) {
+        if (language === "Arabic") {
+            return rle + helloInDifferentLanguages[language] + " " + name + "!";
+        }
         if (language === "Chinese") {
             return helloInDifferentLanguages[language] + name;
         }
